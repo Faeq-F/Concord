4e8569e0033cf8c6c8c053a3477f7ff6834bdb97
Faeq <faeqfaisal@hotmail.co.uk>
2023-12-14 18:57:21 +0000

    Added basic playback functionality of embedded file

--- Concord/app/src/main/java/faeq/concord/MainActivity.kt
+++ Concord/app/src/main/java/faeq/concord/MainActivity.kt
@@ -1,43 +1,39 @@
 package faeq.concord
 
+import android.media.MediaPlayer
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import faeq.concord.ui.theme.ConcordTheme
 
 class MainActivity : ComponentActivity() {
+  
+  private var playing:Boolean = false
+  private lateinit var mediaPlayer: MediaPlayer
+  private lateinit var playPauseButton:Button
+  
   override fun onCreate(savedInstanceState : Bundle?) {
     super.onCreate(savedInstanceState)
-    setContent {
-      ConcordTheme {
-        // A surface container using the 'background' color from the theme
-        Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-          Greeting("Android")
-        }
-      }
-    }
+    setContentView(R.layout.main_activity);
+    playPauseButton = findViewById<View>(R.id.PlayPause) as Button
+    mediaPlayer = MediaPlayer.create(this, R.raw.zealandardor)
+    
   }
-}
-
-@Composable
-fun Greeting(name : String, modifier : Modifier = Modifier) {
-  Text(
-    text = "Hello $name!",
-    modifier = modifier
-  )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-  ConcordTheme {
-    Greeting("Android")
+  
+  override fun finish() {
+    mediaPlayer.stop()
+    mediaPlayer.release()
+    super.finish()
+  }
+  
+  fun playPause(view : View) {
+    if (playing){
+      mediaPlayer.pause()
+      playPauseButton.text = "Play"
+    } else{
+      mediaPlayer.start()
+      playPauseButton.text = "Pause"
+    }
+    playing = !playing
   }
 }
